6131f506a3898b73d2a3272e84f59e16637425ff
Sofiia Hryshko <sofiahryshko@gmail.com>
2026-07-12 14:19:54 +0300

    add table row selection

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
-// write code here
+const tbody = document.querySelector('tbody');
+
+// When the user clicks on a row, it should become selected
+tbody.addEventListener('click', (e) => {
+  const rowClicked = e.target.closest('tr');
+
+  if (!rowClicked) {
+    return;
+  }
+
+  const previousActive = tbody.querySelector('tr.active');
+
+  if (previousActive && previousActive !== rowClicked) {
+    previousActive.classList.remove('active');
+  }
+
+  rowClicked.classList.add('active');
+});
